7ed043cc8334da38d613c65e1d5617c231d7f7f0
Simon Pintarelli <simon.pintarelli@cscs.ch>
2022-05-06 17:57:28 +0200

    remove six / python 2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="upf_to_json",
-    version="0.9.3",
+    version="0.9.4",
     author="Simon Pintarelli",
     author_email="simon.pintarelli@cscs.ch",
     description="upf to json converter",
@@ -19,11 +19,12 @@ setuptools.setup(
         "License :: OSI Approved :: BSD License",
         "Operating System :: OS Independent",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "Topic :: Scientific/Engineering"
-    ],
-    install_requires="six"
+    ]
 )
