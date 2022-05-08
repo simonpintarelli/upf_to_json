887a42d802c8a7295ee0ba2424b319ae0641d758
Simon Pintarelli <simon.pintarelli@cscs.ch>
2022-05-08 17:02:00 +0200

    add python click

--- upf_to_json/upf_to_json.py
+++ upf_to_json/upf_to_json.py
@@ -1,9 +1,7 @@
-import json
-import sys
-import re
-import os
 from .upf1_to_json import parse_upf1_from_string
 from .upf2_to_json import parse_upf2_from_string
+import json
+import click
 
 def get_upf_version(upf):
     lines = upf.split('\n')
@@ -13,50 +11,39 @@ def get_upf_version(upf):
         return 2
     return 0
 
-def parse_upf_from_string(upf_str):
-    version = get_upf_version(upf_str)
-    if version == 0:
-        return None
-    if version == 1:
-        return parse_upf1_from_string(upf_str)
-    if version == 2:
-        return parse_upf2_from_string(upf_str)
 
 def upf_to_json(upf_str, fname):
     """Convert UPF to python dictionary.
-
     :param upf_str: upf as string
     :param fname: filename of the original .UPF file
     """
     version = get_upf_version(upf_str)
-    if version == 0:
-        return None
     if version == 1:
         pp_dict = parse_upf1_from_string(upf_str)
-    if version == 2:
+    elif version == 2:
         pp_dict = parse_upf2_from_string(upf_str)
+    else:
+        raise Exception('Unkown UPF version')
 
     pp_dict['pseudo_potential']['header']['original_upf_file'] = fname
     return pp_dict
 
-def main():
 
-    fname = sys.argv[1]
-    if not os.path.exists(fname):
-        raise FileNotFoundError('invalid path for UPF file')
+@click.group()
+def cli():
+    pass
 
-    with open(sys.argv[1], 'r') as fh:
-        upf_str = fh.read()
 
-    pp_dict = parse_upf_from_string(upf_str)
-    element = pp_dict['pseudo_potential']['header']['element']
-    pp_dict['pseudo_potential']['header']['original_upf_file'] = sys.argv[1]
+@cli.command()
+@click.argument('infile', type=click.File(mode='r'))
+@click.argument('outfile', type=click.File(mode='w'), default='-')
+def convert(infile, outfile):
+    pp_dict = upf_to_json(infile.read(), fname=infile.name)
+    outfile.write(json.dumps(pp_dict))
 
-    with open(element + '.json', 'w') as fout:
-        # Match comma, space, newline and an arbitrary number of spaces ',\s\n\s*' with the
-        # following conditions: a digit before (?<=[0-9]) and a minus or a digit after (?=[-|0-9]).
-        # Replace found sequence with comma and space.
-        fout.write(re.sub(r"(?<=[0-9]),\s\n\s*(?=[-|0-9])", r", ", json.dumps(pp_dict, indent=2)))
 
-if __name__ == "__main__":
-    main()
+@cli.command()
+@click.argument('infile', type=click.File(mode='r'))
+def summary(infile):
+    pp_dict = upf_to_json(infile.read(), fname=infile.name)
+    click.echo(json.dumps(pp_dict['pseudo_potential']['header'], indent=2))
