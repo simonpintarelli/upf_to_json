f95585db7a7daadf863ccff3b13858859368d98f
Simon Pintarelli <simon.pintarelli@cscs.ch>
2024-02-16 13:14:48 +0100

    update pytest

--- tests/test_upf_to_json.py
+++ tests/test_upf_to_json.py
@@ -1,39 +1,49 @@
 import pytest
 
 from upf_to_json import upf_to_json
+from deepdiff import DeepDiff
+import gzip, json
 
-def call_upf2json(fname):
-    """Check that upf_to_json doesn't throw an exception."""
-    try:
-        with open(fname, 'r') as fh:
-            dd = upf_to_json(fh.read(), fname=fname)
-    except:
-        pytest.fail('failed to convert: ', fname)
+
+def diff(fname, ref_json_gz):
+    with open(fname, "r") as fh:
+        dd = upf_to_json(fh.read(), fname=fname)
+    with gzip.open(ref_json_gz, "rt") as fh:
+        dd_ref = json.load(fh)
+    diff = DeepDiff(dd, dd_ref)
+    assert diff == {}
+
 
 def test_gbrv15():
-    fname = 'cr_pbe_v1.5.uspp.F.UPF'
-    call_upf2json(fname)
+    fname = "cr_pbe_v1.5.uspp.F.UPF"
+    diff(fname, fname + ".json.gz")
+
 
 def test_gbrv14():
-    fname = 'cl_pbe_v1.4.uspp.F.UPF'
-    call_upf2json(fname)
+    fname = "cl_pbe_v1.4.uspp.F.UPF"
+    diff(fname, fname + ".json.gz")
+
 
 def test_sg15():
-    fname = 'Kr_ONCV_PBE-1.0.oncvpsp.upf'
-    call_upf2json(fname)
+    fname = "Kr_ONCV_PBE-1.0.oncvpsp.upf"
+    diff(fname, fname + ".json.gz")
+
 
 def test_pseudo_dojo():
-    fname = 'Hf-sp.oncvpsp.upf'
-    call_upf2json(fname)
+    fname = "Hf-sp.oncvpsp.upf"
+    diff(fname, fname + ".json.gz")
+
 
 def test_atompaw():
-    fname = 'Gd.GGA-PBE-paw-v1.0.UPF'
-    call_upf2json(fname)
+    fname = "Gd.GGA-PBE-paw-v1.0.UPF"
+    diff(fname, fname + ".json.gz")
+
 
 def test_pslibrary_paw():
-    fname = 'Al.pbe-n-kjpaw_psl.1.0.0.UPF'
-    call_upf2json(fname)
+    fname = "Al.pbe-n-kjpaw_psl.1.0.0.UPF"
+    diff(fname, fname + ".json.gz")
+
 
 def test_pslibrary_new():
-    fname = 'Ac.us.z_11.ld1.psl.v1.0.0-high.upf'
-    call_upf2json(fname)
+    fname = "Ac.us.z_11.ld1.psl.v1.0.0-high.upf"
+    diff(fname, fname + ".json.gz")
